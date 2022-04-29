48538a72f8ca703641628e86aea0e895cdbf96f1
BrentonPhoenix <brentonphoenix@gmail.com>
2022-04-29 01:36:42 -0400

    further testing of regular equality and changing strings that are math expressions into integers next step is to try looping over the expression

--- Testing/changing data types/stringINT.js
+++ Testing/changing data types/stringINT.js
@@ -1,15 +1,42 @@
-let intString = "12345"
-let intMathString = "1+1"
+// let intString = "12345"
+// let intMathString = "1 + 1"// no difference between "1+1" and "1 + 1" as expected
 
-console.log(intString) // result is 12345 printed to the console
-console.log(intMathString) // result is 1+1 written in the console
+// console.log(intString) // result is 12345 printed to the console
+// console.log(intMathString) // result is 1+1 written in the console
 
 
-console.log(intString * 1)// result is 12345 printed in the console
-console.log(intMathString * 1) // result is NaN 
+// console.log(intString * 1)// result is 12345 printed in the console
+// console.log(intMathString * 1) // result is NaN 
 
-console.log("parseInt intString ",parseInt(intString)) //result 12345
-console.log("parseInt intMathString ",parseInt(intMathString)) //result 1
+// console.log("parseInt intString ",parseInt(intString)) //result 12345
+// console.log("parseInt intMathString ",parseInt(intMathString)) //result 1
 
-console.log('parseFloat intString ', parseFloat(intString))//12345
-console.log('parseFloat intMathString ', parseFloat(intMathString))//1
+// console.log('parseFloat intString ', parseFloat(intString))//12345
+// console.log('parseFloat intMathString ', parseFloat(intMathString))//1
+
+
+// console.log('Number intString',Number(intString))//12345
+// console.log('Number intMathString',Number(intMathString))//NaN
+
+
+
+//it looks like if i want to convert a math expression string(such as "1+1") to integer(s) I will need to loop over it I now see why people love TypeScript. Time to test it out with Multiplication and division
+let intMultiString = "2 * 2"
+let intDiviString = "16 / 2"
+
+console.log(intMultiString)//2 * 2
+console.log(intDiviString) //16 / 2
+
+
+console.log('intMultiString',intMultiString * 1)//NaN
+console.log('intDiviString',intDiviString * 1) //NaN
+
+console.log("parseInt intMultiString ",parseInt(intMultiString))//2 
+console.log("parseInt intDiviString ",parseInt(intDiviString))  //16
+
+console.log('parseFloat intMultiString ', parseFloat(intMultiString))//2
+console.log('parseFloat intDiviString ', parseFloat(intDiviString)) //16
+
+
+console.log('Number intMultiString ',Number(intMultiString))//NaN
+console.log('Number intDiviString ',Number(intDiviString))//NaN
--- Testing/equality/regularEquality.js
+++ Testing/equality/regularEquality.js
@@ -1,4 +1,4 @@
-
+//int
 if (123 == '123'){
     console.log('if statement evaluated to true: regular equality (with single quotes)')
 } else{
@@ -30,6 +30,8 @@ if (true == 'true'){
 //1 == '1' evaluates to true
 
 
+// false
+
 if (false == 'false'){
     console.log('if statement evaluated to true: regular equality (with single quotes)')
 } else{
@@ -49,3 +51,99 @@ if (false == `false`){
 } else{
     console.log('if statement evaluated to false: strict equality (with backticks)')//with regular equality evaluates to false so false is not equal to "false" at all
 }
+
+
+
+
+
+//true
+if (true == 'true'){
+    console.log('if statement evaluated to true: regular equality (with single quotes)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with single quotes)')
+}
+
+
+if (true == "true"){
+    console.log('if statement evaluated to true: regular equality(with double quotes)')
+} else{
+    console.log('if statement evaluated to false: strict regular equality')
+}
+
+
+if (true == `true`){
+    console.log('if statement evaluated to true: regular equality (with backticks)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with backticks)')
+}
+
+
+//null
+
+if (null == 'null'){
+    console.log('if statement evaluated to true: regular equality (with single quotes)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with single quotes)')
+}
+
+
+if (null == "null"){
+    console.log('if statement evaluated to true: regular equality(with double quotes)')
+} else{
+    console.log('if statement evaluated to false: strict regular equality')
+}
+
+
+if (null == `null`){
+    console.log('if statement evaluated to true: regular equality (with backticks)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with backticks)')
+}
+//all evaluate to false
+
+
+//undefined
+if (undefined == 'undefined'){
+    console.log('if statement evaluated to true: regular equality (with single quotes)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with single quotes)')
+}
+
+
+if (undefined == "undefined"){
+    console.log('if statement evaluated to true: regular equality(with double quotes)')
+} else{
+    console.log('if statement evaluated to false: strict regular equality')
+}
+
+
+if (undefined == `undefined`){
+    console.log('if statement evaluated to true: regular equality (with backticks)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with backticks)')
+}
+
+//all evaluate to false
+
+
+//NaN
+if (NaN == 'NaN'){
+    console.log('if statement evaluated to true: regular equality (with single quotes)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with single quotes)')
+}
+
+
+if (NaN == "NaN"){
+    console.log('if statement evaluated to true: regular equality(with double quotes)')
+} else{
+    console.log('if statement evaluated to false: strict regular equality')
+}
+
+
+if (NaN == `NaN`){
+    console.log('if statement evaluated to true: regular equality (with backticks)')
+} else{
+    console.log('if statement evaluated to false: strict equality (with backticks)')
+}
+
